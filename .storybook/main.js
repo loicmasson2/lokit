b52084a8b9735fac650d015f9488b065d0f5b97c
Loïc Masson <masson.loic.023@gmail.com>
2022-02-05 16:38:34 +0200

    update all repo to use src, main, module

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,4 +6,30 @@ module.exports = {
     "@storybook/addon-actions",
   ],
   framework: "@storybook/react",
+  webpackFinal: async (config) => {
+    // remove default css rule from storybook
+    config.module.rules = config.module.rules.filter(
+      (f) => f.test.toString() !== "/\\.css$/"
+    );
+
+    // push our custom easy one
+    config.module.rules.push({
+      test: /\.css$/,
+      use: [
+        "style-loader",
+        {
+          loader: "css-loader",
+          options: {
+            // Key config
+            modules: true,
+          },
+        },
+      ],
+    });
+    // This is where we change the order of resolution of main fields
+    config.resolve.mainFields = ["src", "module", "main"];
+
+    // Return the altered config
+    return config;
+  },
 };
